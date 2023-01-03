1bac9ea87e75cb1648b612579e502f3c63023e7e
EC2 Default User <ec2-user@ip-172-31-83-90.ec2.internal>
2023-01-03 12:14:33 +0000

    finish 1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
   def index
     @book = Book.new #新規投稿用
     @books = Book.all
@@ -7,15 +8,44 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      @user = User.find(current_user.id)
+      render :index
+    end
   end
-
   def show
+    @books = Book.find(params[:id])
+    @user = @books.user
+    # @user = User.find(@books.user_id)
+    @book = Book.new
+  end
+  def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  def correct_user
+    book = Book.find(params[:id])
+    user = book.user
+    redirect_to(books_path) unless user == current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :books, only: [:index, :show, :new, :create]
-  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :new, :create, :destroy, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
