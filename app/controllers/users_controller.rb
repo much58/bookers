fe94224abd0b92862de6216862b0d8e3b68a3546
EC2 Default User <ec2-user@ip-172-31-83-90.ec2.internal>
2023-01-05 10:21:52 +0000

    [finish]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,6 @@ class UsersController < ApplicationController
   end
   def correct_user
     user = User.find(params[:id])
-    redirect_to(users_path) unless user == current_user
+    redirect_to(user_path(current_user.id)) unless user == current_user
   end
 end
